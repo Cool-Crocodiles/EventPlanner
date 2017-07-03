454aad37740da4b543339e269f4310fbf1490705
Taras Ignashchenko <tignashchenko@gmail.com>
2017-07-03 14:15:04 -0700

    July 3 client-side code cleanup

--- react-client/src/components/EventDescriptionPage/EventDescriptionPage.jsx
+++ react-client/src/components/EventDescriptionPage/EventDescriptionPage.jsx
@@ -4,7 +4,7 @@ import $ from 'jquery';
 import images from './imageSample.js'
 
 class EventDescriptionPage extends React.Component {
-  
+
   constructor(props) {
     super(props);
   }
@@ -14,11 +14,6 @@ class EventDescriptionPage extends React.Component {
   }
 
   render () {
-    var des = this.props.selectedEvent.description; 
-    var div = document.createElement("div");
-    div.innerHTML = des; 
-    console.log('INNERHTML::', div.innerHTML);
-
     return (
       <div id="wrapUpTwoSection">
         <section id="eventDescriptionInfoText">
@@ -27,20 +22,20 @@ class EventDescriptionPage extends React.Component {
             { this.props.eventCategory === "PerformingArts" ? <img id="eventImage" src= {images.arts[this.props.indexForImages]} /> : null} <br/>
             { this.props.eventCategory === "Sports" ? <img id="eventImage" src= {images.sports[this.props.indexForImages]} /> : null} <br/>
             { this.props.eventCategory === "" ? <img id="eventImage" src= {images.sports[this.props.indexForImages]} /> : null} <br/>
-          <div className="selectedData"> 
+          <div className="selectedData">
             <b>Description: </b>{ this.props.selectedEvent.description }<br/>
           </div>
-          <div className="selectedData"> 
+          <div className="selectedData">
             <b>Find Tickets: </b>
             <a href={this.props.selectedEvent.url} target="_blank">{this.props.selectedEvent.url}</a><br/>
           </div>
-          <div className="selectedData"> 
+          <div className="selectedData">
             <b>Location: </b>{this.props.selectedEvent.venue_name}<br/>
           </div>
-          <div className="selectedData"> 
+          <div className="selectedData">
             <b>Address: </b>{this.props.selectedEvent.venue_address}<br/>
           </div>
-          <div className="selectedData" id="map"> 
+          <div className="selectedData" id="map">
             <b>Map: </b>
             <a href={this.props.selectedEvent.venue_url} target="_blank">{this.props.selectedEvent.venue_url}</a><br/>
           </div>
